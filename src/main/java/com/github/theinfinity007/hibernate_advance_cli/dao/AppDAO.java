750714ad756aa826b53e7ceff106a4dca0faf6a7
Ram Chandra <46575254+TheInfinity007@users.noreply.github.com>
2025-08-31 22:57:33 +0530

    Create App DAO

---
+++ src/main/java/com/github/theinfinity007/hibernate_advance_cli/dao/AppDAO.java
@@ -0,0 +1,7 @@
+package com.github.theinfinity007.hibernate_advance_cli.dao;
+
+import com.github.theinfinity007.hibernate_advance_cli.entity.Instructor;
+
+public interface AppDAO {
+    void save(Instructor instructor);
+}
---
+++ src/main/java/com/github/theinfinity007/hibernate_advance_cli/dao/AppDAOImpl.java
@@ -0,0 +1,22 @@
+package com.github.theinfinity007.hibernate_advance_cli.dao;
+
+import com.github.theinfinity007.hibernate_advance_cli.entity.Instructor;
+import jakarta.persistence.EntityManager;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.annotation.Transactional;
+
+public class AppDAOImpl implements AppDAO {
+
+    private EntityManager entityManager;
+
+    @Autowired
+    public AppDAOImpl(EntityManager entityManager) {
+        this.entityManager = entityManager;
+    }
+
+    @Override
+    @Transactional
+    public void save(Instructor instructor) {
+        entityManager.persist(instructor);
+    }
+}
